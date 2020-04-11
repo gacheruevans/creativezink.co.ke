39b7c4103744583cae3e504ad0c803a685552007
egacheru <gacheruevans0@gmail.com>
2020-04-11 20:58:18 +0300

    adds redux features

--- app/dev/js/index.js
+++ app/dev/js/index.js
@@ -1,7 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import allReducers from './reducers';
+import App from './components/App';
 
-const store = createStore();
+const store = createStore(allReducers);
 
-ReactDOM.render(<h2>Hey Now</h2>,document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,document.getElementById('root'));
---
+++ app/dev/js/reducers/reducer-readings.js
@@ -0,0 +1,34 @@
+export default function() {
+    return [
+        {
+            id: 1,
+            temperature: "38.0c",
+            humidity: "72"
+        },
+        {
+            id: 2,
+            temperature: "39.0c",
+            humidity: "75"
+        },
+        {
+            id: 3,
+            temperature: "38.0c",
+            humidity: "72"
+        },
+        {
+            id: 4,
+            temperature: "36.0c",
+            humidity: "62"
+        },
+        {
+            id: 5,
+            temperature: "37.0c",
+            humidity: "68"
+        },
+        {
+            id: 6,
+            temperature: "40.0c",
+            humidity: "80"
+        }
+    ]
+}
